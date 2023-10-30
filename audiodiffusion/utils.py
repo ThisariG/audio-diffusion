b78c5e953351a2dfb3e25995457ab8abcfeef5af
R.T.I.Gunawardena <62197186+ThisariG@users.noreply.github.com>
2023-10-30 14:27:03 +0530

    Update utils.py

--- audiodiffusion/utils.py
+++ audiodiffusion/utils.py
@@ -124,7 +124,7 @@ def conv_attn_to_linear(checkpoint):
         if ".".join(key.split(".")[-2:]) in attn_keys:
             if checkpoint[key].ndim > 2:
                 checkpoint[key] = checkpoint[key][:, :, 0, 0]
-        elif "proj_attn.weight" in key:
+        elif "proj_out.weight" in key:
             if checkpoint[key].ndim > 2:
                 checkpoint[key] = checkpoint[key][:, :, 0]
 
